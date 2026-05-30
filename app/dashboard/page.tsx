0ce174436043827db4bab9d7e66fad0ffa9ec7bb
Dorian-devw <frank.sinca@tecsup.edu.pe>
2026-05-30 13:45:16 -0500

    Correciones

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -352,8 +352,6 @@ function DashboardContent(){
                       </div>
                     </div>
                   </CardContent>
-                </Card>
-              ))}
                   </Card>
                 ))}
               </div>
